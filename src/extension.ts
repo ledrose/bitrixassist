5a09a6127e1707d640f79b1ae60e163ff2e8d99a
Egor <eev@sibcode.team>
2026-03-28 22:58:47 +0700

    Parse php to extract args

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,117 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+const engine = require("php-parser");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function handleParse(node: any) {
+	const results: any = [];
+	function traverse(node: any) {
+		if (!node) return;
+		if (node.kind == "expressionstatement") {
+			return traverse(node.expression);
+		}
+		if (node.kind == "call" &&
+			node.what?.kind == "propertylookup" &&
+			node.what?.offset?.name == "IncludeComponent" &&
+			node.arguments?.length >= 2
+		) {
+			let localResult = {arg1: {}, arg2: {}};
+			if (node.arguments[0]?.kind == "string") {
+				const argData = node.arguments[0];
+				localResult['arg1'] = {
+					value: argData.value,
+					start: argData.loc?.start,
+					end: argData.loc?.end
+				}
+			}	
+			if (node.arguments[1]?.kind == "string") {
+				const argData = node.arguments[1];
+				localResult['arg2'] = {
+					value: argData.value,
+					start: argData.loc?.start,
+					end: argData.loc?.end
+				}
+			}	
+			if (localResult['arg1'] || localResult['arg2']) {
+				results.push(localResult);
+			}
+		}	
+
+
+		if (node.children) {
+			for (let i = 0; i < node.children.length; i++) {
+				const child = node.children[i];
+				traverse(child);
+			}
+		}
+		if (node.body?.children) {
+			for (let i = 0; i < node.body.children.length; i++) {
+				const child = node[i];
+				traverse(child);
+			}
+		}
+	}
+
+	traverse(node);
+	return results;
+}
+
+function findBitrixRoot(filePath: string) {
+
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "bitrixassist" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('bitrixassist.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from bitrixAssist!');
 	});
-
 	context.subscriptions.push(disposable);
+
+
+	const parser = new engine({
+		ast: {
+			withPositions: true
+		},
+		lexer: {
+			short_tags: true
+		}
+	})
+	const linkProvider: vscode.DocumentLinkProvider = {provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
+		console.log("LinkProvider trigger");
+		const links: vscode.DocumentLink[] = [];
+        const text = document.getText();
+		const code = parser.parseCode(text);
+		const data = handleParse(code);
+		
+		let loc, start, end, targeUrl;
+		data.forEach((el: any) => {
+			// new vscode.Position()
+			if (el.arg1) {
+				start = new vscode.Position(el.arg1.start.line-1,el.arg1.start.column);
+				end = new vscode.Position(el.arg1.end.line-1,el.arg1.end.column);
+				loc = new vscode.Range(start,end);
+				targeUrl = vscode.Uri.parse("https://google.com");
+				links.push(new vscode.DocumentLink(loc, targeUrl))
+			}
+			if (el.arg2) {
+				start = new vscode.Position(el.arg2.start.line-1,el.arg2.start.column);
+				end = new vscode.Position(el.arg2.end.line-1,el.arg2.end.column);
+				loc = new vscode.Range(start,end);
+				targeUrl = vscode.Uri.parse("https://microsoft.com");
+				links.push(new vscode.DocumentLink(loc, targeUrl))
+			}
+		});
+
+		return links;
+	}};
+
+	const selector: vscode.DocumentSelector = {'language': 'php'};
+
+	let provider = vscode.languages.registerDocumentLinkProvider(selector, linkProvider)
+	context.subscriptions.push(provider);
+
 }
 
 // This method is called when your extension is deactivated
